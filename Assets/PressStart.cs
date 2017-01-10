25a9f59202c8e86afc223f29613117509e7d79aa
Matt Spaulding <matthew.spaulding@iprocedures.com>
2017-01-10 00:55:54 -0500

    Added title screen.

---
+++ Assets/PressStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float space = Input.GetAxis("Jump"); //break control input
+		if (space>0) {
+			Application.LoadLevel ("Game/CharacterSelect");
+		}
+		
+	}
+}
